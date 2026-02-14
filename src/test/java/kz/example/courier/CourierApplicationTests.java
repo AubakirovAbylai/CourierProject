0d9d6a379e947d3680b9c5af8eee90331193b6d1
Abylai Aubakirov <abl110596gmail.com>
2026-02-14 13:29:55 +0500

    first commit

---
+++ src/test/java/kz/example/courier/CourierApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.example.courier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
